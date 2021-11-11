a9f56904013add9ab40090514c6981162844ac92
S G <serbangilvitu@users.noreply.github.com>
2021-11-11 22:21:48 +0400

    Added python API draft

--- README.md
+++ README.md
@@ -27,11 +27,13 @@ cd api
 apt install python3.8-venv
 python3 -m venv venv
 source venv/bin/activate
-pip install flask pika
+pip install flask pika urllib3
 pip freeze > requirements.txt
 ```
 #### Running the application
 ```
+docker run -d --name rmq --restart -p 5672:5672 -p 15672:15672 rabbitmq:3.7.0-management
+
 FLASK_APP=api.py \
 FLASK_ENV=development \
 flask run
--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import json
+import urllib3
 
 app = Flask(__name__)
-
+http = urllib3.PoolManager()
+headers = urllib3.util.make_headers(basic_auth='guest:guest')
 
 @app.route('/')
 def root():
@@ -14,4 +17,12 @@ def root():
 
 @app.route('/fetch')
 def fetch():
-    return 'FETCH'
+    resp = http.request('GET', 'http://127.0.0.1:15672/api/queues',headers=headers)
+    queue_list = []
+    data = json.loads(resp.data)
+    print("skeptical")
+    print(data)
+    for i in range(len(data)):
+        queue_list.append(data[i]['name'])
+    print(queue_list)
+    return json.dumps(queue_list)
