e60becb2ae91f56594ab7c712ad8bc10bb75a999
S G <serbangilvitu@users.noreply.github.com>
2021-11-15 22:40:52 +0400

    Updated FetchPage

--- web/src/components/FetchPage.js
+++ web/src/components/FetchPage.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import Select from 'react-select'
 import axios from 'axios'
+import Navbar from './Navbar';
 
 export default class FetchPage extends Component {
 
@@ -40,10 +41,38 @@ export default class FetchPage extends Component {
   render() {
     console.log(this.state.selectOptions)
     return (
+    <div>
       <div>
-        <Select options={this.state.selectOptions} onChange={this.handleChange.bind(this)}/>
-    <p>You have selected <strong>{this.state.name}</strong></p>
+        <Navbar />
       </div>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <div style={{ width: "20em" }}>
+                  <Select options={this.state.selectOptions} onChange={this.handleChange.bind(this)}/>
+                </div>
+              </td>
+              <td>
+                <p>placeholder</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label for="quantity">Quantity (between 1 and 10): </label>
+                <input type="number" id="quantity" name="quantity" min="1" max="10"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div><b>Selected Value: </b> {this.state.name}</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     )
   }
 }
