f04183ef175d2f21233cfbcf59dfc345e71b5b91
S G <serbangilvitu@users.noreply.github.com>
2021-12-06 21:30:02 +0400

    Updated css

--- web/src/components/InsertPage.js
+++ web/src/components/InsertPage.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import Select from 'react-select'
 import axios from 'axios'
 import Navbar from './Navbar';
+import './page.css';
 
 export default class InsertPage extends Component {
 
@@ -79,7 +80,7 @@ export default class InsertPage extends Component {
             <Navbar />
           </div>
           <div style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
-            <div style={{ left: '0vmin', top: '0vmin', backgroundColor: '#d27979', fontSize: 'calc(10px + 1vmin)' }}>
+            <div style={{ left: '0vmin', top: '0vmin', backgroundColor: '#99ccff', fontSize: 'calc(10px + 1vmin)' }}>
               <table>
                 <tbody>
                   <tr>
@@ -92,18 +93,9 @@ export default class InsertPage extends Component {
                       </div>
                     </td>
                     <td>
-                      <button
+                      <button className="orange-button"
                         onClick={this.handleButtonClick.bind(this)}
-                        style={{
-                          paddingLeft: '2vmin',
-                          outline: '0',
-                          boxShadow: '0px 2px 2px lightblue',
-                          transition: 'ease background-color 250ms',
-                          borderRadius: '5px',
-                          fontSize: 'calc(10px + 1vmin)',
-                          backgroundColor: '#3f51b5',
-                          color: 'white'
-                        }}
+
                       >Insert message</button>
                     </td>
                   </tr>
