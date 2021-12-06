d79d9f07f461f0f36281a66ff0be12a1d707d93d
S G <serbangilvitu@users.noreply.github.com>
2021-12-06 21:54:10 +0400

    InsertPage - log responses

--- web/src/components/InsertPage.js
+++ web/src/components/InsertPage.js
@@ -13,7 +13,8 @@ export default class InsertPage extends Component {
       selectOptions: [],
       queueAttributes: [],
       queueName: '',
-      message: ''
+      message: '',
+      response: ''
     }
   }
 
@@ -56,16 +57,24 @@ export default class InsertPage extends Component {
       headers: { 'Content-Type': 'multipart/form-data' }
     }
     console.log(this.state.message)
-    let formData = new FormData(); 
-    formData.append('message', this.state.message)
-    formData.append('queueName', this.state.queueName)
-    formData.append('durable', this.state.queueAttributes[this.state.queueName]['durable'])
-    axios.post('http://localhost:8080/insert-message', formData, config)
-    .then(response => {
-      return response.data;
-    }).catch(error => {
-      return error;
-    });
+    let formData = new FormData();
+    let now = new Date();
+    try {
+      formData.append('message', this.state.message)
+      formData.append('queueName', this.state.queueName)
+      formData.append('durable', this.state.queueAttributes[this.state.queueName]['durable'])
+      axios.post('http://localhost:8080/insert-message', formData, config)
+        .then(response => {
+          this.setState({ response: `successfully inserted at ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}` })
+          return response.data;
+        }).catch(error => {
+          return error;
+        });
+    }
+    catch(err) {
+      console.log(err)
+      this.setState({ response: `error "${err}" at ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}` })
+    }
   }
 
   componentDidMount() {
@@ -76,43 +85,43 @@ export default class InsertPage extends Component {
     console.log(this.state.selectOptions)
     return (
       <div>
-          <div>
-            <Navbar />
-          </div>
-          <div style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
-            <div style={{ left: '0vmin', top: '0vmin', backgroundColor: '#99ccff', fontSize: 'calc(10px + 1vmin)' }}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>
-                      <p>Queue</p>
-                    </td>
-                    <td>
-                      <div style={{ width: "50vmin" }}>
-                        <Select id="queue" options={this.state.selectOptions} onChange={this.handleQueueChange.bind(this)} />
-                      </div>
-                    </td>
-                    <td>
-                      <button className="orange-button"
-                        onClick={this.handleButtonClick.bind(this)}
+        <div>
+          <Navbar />
+        </div>
+        <div style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
+          <div style={{ left: '0vmin', top: '0vmin', backgroundColor: '#99ccff', fontSize: 'calc(10px + 1vmin)' }}>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <p>Queue</p>
+                  </td>
+                  <td>
+                    <div style={{ width: "50vmin" }}>
+                      <Select id="queue" options={this.state.selectOptions} onChange={this.handleQueueChange.bind(this)} />
+                    </div>
+                  </td>
+                  <td>
+                    <button className="orange-button"
+                      onClick={this.handleButtonClick.bind(this)}
 
-                      >Insert message</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div>
-              <p>{this.state.message}</p>
-              <textarea
-                name='message'
-                style={{ height: '100%', width: '100%', left: '0em', top: '0em' }}
-                rows={50}
-                cols={100}
-                onChange={this.handleMessageChange.bind(this)}
-              />
-            </div>
+                    >Insert message</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+          <div>
+            <p>{this.state.response}</p>
+            <textarea
+              name='message'
+              style={{ height: '100%', width: '100%', left: '0em', top: '0em' }}
+              rows={50}
+              cols={100}
+              onChange={this.handleMessageChange.bind(this)}
+            />
+          </div>
+        </div>
       </div>
     )
   }
