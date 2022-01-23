baab6c805b087d213a59308b55d4dc98ba762c89
S G <serbangilvitu@users.noreply.github.com>
2022-01-24 00:00:15 +0400

    Updated InsertPage,MovePage

--- web/src/components/MovePage.js
+++ web/src/components/MovePage.js
@@ -21,25 +21,47 @@ export default class FetchPage extends Component {
   }
 
   async getOptions() {
-    const res = await axios.get('http://localhost:8080/queue-list')
-    const data = res.data
 
-    const options = data.map(d => ({
-      "value": d.name,
-      "label": d.name
-    }))
-
-    let attributes = {}
-    console.log(data)
-    for (let k in Object.keys(data)) {
-      console.log(`${data[k]['name']} ${data[k]['durable']}`)
-      attributes[data[k]['name']] = {}
-      attributes[data[k]['name']]['durable'] = data[k]['durable']
+    let config = {
+      headers: { 'Content-Type': 'multipart/form-data' }
     }
+    let formData = new FormData();
+    try {
+      formData.append('mqUser', sessionStorage.getItem('username'))
+      formData.append('mqPassword', atob(sessionStorage.getItem('password')))
+      axios.post('http://localhost:8080/queue-list', formData, config)
+        .then(response => {
+          if (response.data.length === 0) {
+            this.setState({ status: `no queues found` })
+          }
+          const data = response.data
 
-    this.setState({ selectOptions: options })
-    this.setState({ queueAttributes: attributes })
+          const options = data.map(d => ({
+            "value": d.name,
+            "label": d.name
+          }))
 
+          let attributes = {}
+          console.log(data)
+          for (let k in Object.keys(data)) {
+            console.log(`${data[k]['name']} ${data[k]['durable']}`)
+            attributes[data[k]['name']] = {}
+            attributes[data[k]['name']]['durable'] = data[k]['durable']
+          }
+
+          this.setState({ selectOptions: options })
+          this.setState({ queueAttributes: attributes })
+          return response;
+        }).catch(error => {
+          this.setState({ status: `error "${error}" at ${this.getTimestamp()}` })
+          console.log(error)
+          return error;
+        });
+    }
+    catch (err) {
+      console.log(err)
+      this.setState({ status: `error "${err}" at ${this.getTimestamp()}` })
+    }
   }
 
   getTimestamp() {
